25af6df6310f94073cfd43a7393f7cb551704294
Jeroen Faijdherbe <jeroen@thisisdevelopment.nl>
2024-12-03 13:45:10 +0100

    Day-03-2

--- day-03.el
+++ day-03.el
@@ -2,19 +2,38 @@
 
 (require 'advent-of-code)
 
-;; xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-
-(setq aoc-mul-regexp "mul(\\([0-9]+\\),\\([0-9]+\\))")
-
-(defun aoc-parse-mul (line &optional start)
-  (when (string-match-p aoc-mul-regexp line start)
-    (let* ((offset (string-match aoc-mul-regexp line start))
-	   (match (match-string 0 line))
-	   (a (string-to-number (match-string 1 line)))
-	   (b (string-to-number (match-string 2 line)))
-	   (rest (aoc-parse-mul (substring line (+ offset (length match))))))
-      (+ (* a b) (or rest 0)))))
+;;; Part 1
+(defun aoc-parse-mul (line)
+  (let ((aoc-mul-regexp "mul(\\([0-9]+\\),\\([0-9]+\\))"))
+    (when (string-match-p aoc-mul-regexp line)
+      (let* ((offset (string-match aoc-mul-regexp line))
+	     (match (match-string 0 line))
+	     (a (string-to-number (match-string 1 line)))
+	     (b (string-to-number (match-string 2 line)))
+	     (rest (aoc-parse-mul (substring line (+ offset (length match))))))
+        (+ (* a b) (or rest 0))))))
 
 
 (aoc-answer (apply '+ (mapcar 'aoc-parse-mul
 			      (aoc-read-input))))
+
+  
+;;; Part 2
+(let ((line (string-join (aoc-read-input) ""))
+      (enabled t)
+      (regexp "mul(\\([0-9]+\\),\\([0-9]+\\))\\|do()\\|don't()")
+      (total 0))
+  (while (string-match-p regexp line)
+    (let ((offset (string-match regexp line))
+          (match (match-string 0 line)))
+      (cond ((string= "do()" match)
+             (setq enabled t))
+            ((string= "don't()" match)
+             (setq enabled nil))
+            (enabled
+             (let ((a (string-to-number (match-string 1 line)))
+	           (b (string-to-number (match-string 2 line))))
+               (setq total (+ total (* a b))))))
+      (setq line (substring line (+ offset (length match))))))
+  (aoc-answer total))
+          
