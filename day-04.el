0c5bebfdb1ce5e8d7497b63c4b1b52965755e558
Jeroen Faijdherbe <jeroen@faijdherbe.net>
2024-12-04 09:36:26 +0100

    Day 4 -1

---
+++ day-04.el
@@ -0,0 +1,59 @@
+;; -*- aoc-date: (2024 4); -*-
+
+(require 'advent-of-code)
+
+(defun aoc-build-map (input)
+  `((map . ,input)
+    (dim . ((w . ,(length (nth 0 input)))
+	    (h . ,(length input))))))
+
+
+(defun aoc-char-from-map (map loc)
+  (or (let-alist map
+    (let ((x (car loc))
+	  (y (cdr loc)))
+      (when (and (> .dim.w x)
+		 (>= x 0)
+		 (> .dim.h y)
+		 (>= y 0))
+	(elt (nth y .map) x))))
+      0))
+
+(defun aoc-translate (loc dir)
+  (cons (+ (car loc) (car dir))
+	(+ (cdr loc) (cdr dir))))
+
+(defun aoc-read-word (map loc dir len)
+  (let ((c (aoc-char-from-map map loc)))
+    (if (> (1- len) 0)
+	(cons c (aoc-read-word map
+			       (aoc-translate loc dir)
+			       dir
+			       (1- len)))
+      (cons c nil))))
+
+(defun aoc-find-occurances (word map)
+  (let ((result '()))
+    (let-alist map
+      (dotimes (y .dim.h)
+	(dotimes (x .dim.w)
+	  (when (eq ?X (aoc-char-from-map map (cons x y)))
+	    (dolist (dir '((1 . 0) (1 . 1) (0 . 1) (-1 . 1) (-1 . 0) (-1 . -1) (0 . -1) (1 . -1)))
+	      (when (string= word (concat (aoc-read-word map
+						 (cons x y)
+						 dir
+						 (length word))))
+		(add-to-list 'result `((loc . ,(cons x y))
+				       (dir . ,dir)))))))))
+    result))
+
+
+
+
+;;; Part A
+(aoc-answer
+ (length
+  (aoc-find-occurances
+   "XMAS"
+   (aoc-build-map (aoc-read-input)))))
+
