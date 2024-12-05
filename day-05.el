a599abc224987954740abd14f5cb3654c65e53c2
Jeroen Faijdherbe <jeroen@faijdherbe.net>
2024-12-05 14:08:32 +0100

    Day 05 B

--- day-05.el
+++ day-05.el
@@ -2,6 +2,7 @@
 
 (require 'advent-of-code)
 
+;;; Part A
 (defun aoc-parse-d5 (input)
   (let ((parts (split-string input "\n\n" t)))
     `((rules . ,(split-string (car parts) "\n" t))
@@ -15,20 +16,22 @@
 
 (defun aoc-parse-updates (updates)
   (mapcar (lambda (upd)
-	    (reverse (split-string upd "," t)))
+	    (split-string upd "," t))
 	    updates))
 
-(defun aoc-filter-updates (updates rules)
+(defun aoc-filter-updates (updates rules &optional inverse)
   (seq-filter (lambda (update)
-		(let ((ok t)
-		      (k (car update)))
+		(let* ((update (reverse update))
+		       (ok t)
+		       (k (car update)))
 		  (while (and ok k)
 		    (setq ok 
 			  (= 0 (length (seq-intersection (gethash k rules)
 							 (cdr update)))))
 		    (setq update (cdr update))
 		    (setq k (car update)))
-		  ok))
+		  (if inverse (not ok)
+		    ok)))
 	      updates))
 
 (defun aoc-take-middle (seq)
@@ -42,6 +45,23 @@
 	  (middles (mapcar 'aoc-take-middle valid-updates))
 	  (middles (mapcar 'string-to-number middles)))
      (apply '+ middles))))
-	  
-     
-			
+
+;;; Part B
+
+(defun aoc-sort-update (update rules)
+  (sort update (lambda (a b)
+	  (member b (gethash a rules)))))
+
+(aoc-answer
+ (let-alist (aoc-read-input nil 'aoc-parse-d5)
+   (let* ((rules (aoc-parse-rules .rules))
+	  (updates (aoc-parse-updates .updates))
+	  (updates (aoc-filter-updates updates rules t))
+	  (updates (mapcar (lambda (update)
+			     (aoc-sort-update update rules))
+			   updates))
+	  (middles (mapcar 'aoc-take-middle updates))
+	  (middles (mapcar 'string-to-number middles)))
+     (apply '+ middles))))
+
+
