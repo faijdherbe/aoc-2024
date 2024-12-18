5a7ebe902f8a13c32b5b2220ef072f82ca5158dd
Jeroen Faijdherbe <jeroen@faijdherbe.net>
2024-12-18 19:12:43 +0100

    d18b

--- day-18.el
+++ day-18.el
@@ -27,5 +27,41 @@
 		    (1- (cdr bounds)))))
    (let-alist (dijkstra map start) 
      (gethash exit .dist))))
-		
+
+
+
+
+;; part b
+(defun aoc-find-exit (map start exit)
+  (let-alist (dijkstra map start) 
+    ((gethash exit .dist)))
+
+(let* ((input (aoc-read-input))
+       (bounds '(71 . 71))
+       (start '(0 . 0))		 
+       (exit (cons (1- (car bounds))
+		   (1- (cdr bounds))))
+       (op '+)
+       (offset 1204)
+       (stepsize (round (/ (- (length input) offset) 2)))
+       (d 0))
+  (while (> stepsize 0)
+    (message "offset: %d(%d), op: %S, stepsize: %d, d: %d" offset (length input) op stepsize d)
+    (let ((dist (aoc-find-exit (aoc-parse-map (seq-take input offset) bounds) start exit)))
+      (when dist
+	(setq d dist))
+
+      (when (or (and dist
+		     (eq '- op))
+		(and (not dist)
+		     (eq '+ op)))
+	(setq stepsize (round (/ stepsize 2)))
+	(setq op (if (eq '+ op)
+		     '- '+)))
+      
+      (setq offset (apply op (list offset stepsize)))))
+  (aoc-answer (nth (1- offset) input)))
+
+  
+ 
   
