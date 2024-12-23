fadbf3a3f7fa7d5a191068a00064038b8dfe144d
Jeroen Faijdherbe <jeroen@thisisdevelopment.nl>
2024-12-23 14:55:07 +0100

    more partial d23a

--- day-23.el
+++ day-23.el
@@ -2,24 +2,76 @@
 
 (require 'advent-of-code)
 
+(setq aoc-input '(
+"kh-tc"
+"qp-kh"
+"de-cg"
+"ka-co"
+"yn-aq"
+"qp-ub"
+"cg-tb"
+"vc-aq"
+"tb-ka"
+"wh-tc"
+"yn-cg"
+"kh-ub"
+"ta-co"
+"de-co"
+"tc-td"
+"tb-wq"
+"wh-td"
+"ta-ka"
+"td-qp"
+"aq-cg"
+"wq-ub"
+"ub-vc"
+"de-ta"
+"wq-aq"
+"wq-vc"
+"wh-yn"
+"ka-de"
+"kh-ta"
+"co-tc"
+"wh-qp"
+"tb-vc"
+"td-yn"
+))
+
 (defun aoc-register-pair (pair table)
   (puthash (car pair)
-	   (add-to-list 
-	    (gethash (car pair)
-		     table
-		     nil)
-		  (cdr pair))
+	   (push (cdr pair)
+	         (gethash (car pair)
+		          table
+		          nil))
 	   table)
   (puthash (cdr pair)
-	   (add-to-list
-	    (gethash (cdr pair)
-		     table
-		     nil)
-	    (car pair))
+	   (push (car pair)
+	         (gethash (cdr pair)
+		          table
+		          nil))
 	   table))
 
+
+(defun aoc-chief-group-p (g)
+  (length> (seq-filter (lambda (c)
+                         (= ?t (elt c 0)))
+                       g)
+           0))
+
+(defun aoc-map-networks (table)
+  (let (out (keys (hash-table-keys table)))
+    (while keys
+      (let* ((key (car keys))
+             (nkey (cadr keys))
+             (int (seq-intersection (cons key (gethash key table))
+                                    (cons nkey (gethash nkey table)))))
+        (setq keys (cdr keys))
+        (when (= 3 (length int))
+          (add-to-list 'out int 'string<))))
+      out))
+
 (let* ((table (make-hash-table :test 'equal))
-       (input (aoc-read-input))
+       (input aoc-input)
        (input (mapcar (lambda (l)
 			(let ((ll (string-split l "-")))
 			  (cons (car ll)
@@ -29,11 +81,11 @@
 			(aoc-register-pair x table)
 			x)
 		      input)))
-  (let ((candidates (seq-filter (lambda (c)
-				  (= ?t (elt c 0)))
-				(hash-table-keys table))))
-    (aoc-answer
-     (length
-      (seq-filter (lambda (n)
-		    (= 2 (length (seq-uniq (gethash n table) ))))
-		  candidates)))))
+  (let* ((networks (aoc-map-networks table))
+         (groups (seq-filter (lambda (g)
+                                      (not (member nil g)))
+                                    networks))
+         (groups (seq-filter 'aoc-chief-group-p
+                             groups)))
+    (debug groups)
+    (aoc-answer (length groups))))
