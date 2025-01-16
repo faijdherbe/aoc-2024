850ac4d956785a7d7fdfa4e4ee4074646a7b65a0
Jeroen Faijdherbe <jeroen@thisisdevelopment.nl>
2025-01-16 11:49:21 +0100

    day25 but smaller

---
+++ day-25-2.el
@@ -0,0 +1,23 @@
+;; -*- aoc-date: (2024 25); -*-
+
+(require 'advent-of-code)
+
+(defun ~parse-part (data)
+  (seq-reduce (lambda (carry value)
+                (logior (lsh carry 1)
+                        (if (= ?# value) 1 0)))
+              (substring (string-join data) 4 -4) 0))
+
+(let ((data (aoc-read-input))
+      (objects '())
+      (pairs 0))
+  (while (car data)
+    (setq objects (push (~parse-part (take 7 data)) objects))
+    (setq data (nthcdr 7 data)))
+  (while (cdr objects)
+    (let ((a (car objects)))
+      (dolist (b (cdr objects))
+        (when (= 0 (logand a b))
+          (setq pairs (1+ pairs)))))
+    (setq objects (cdr objects)))
+  pairs)
