3256971894383483705cee61bc42418ed95707b7
Jeroen Faijdherbe <jeroen@thisisdevelopment.nl>
2025-01-20 12:38:52 +0100

    day 25 in PHP.

---
+++ day-25.php
@@ -0,0 +1,16 @@
+<?php
+
+fputs($fp = fopen('php://memory', 'r+'), preg_replace(
+    ['/[^\\.#]/', '/\\./', '/#/'],
+    ['','0','1'],
+    file_get_contents(__DIR__ . '/input/25.txt'))
+); rewind($fp);
+
+$objects = []; $count = 0;
+
+while($object = fgets($fp, 7*5+1)) {
+    $a = $objects[] = bindec(substr($subs = $object, 4, -4));
+    foreach ($objects as $b) $count += ($a+$b) == ($a ^ $b);
+}
+
+echo $count . PHP_EOL;
